04ec5452bc9cc543d61b506ca0d06fe047e7f673
Panyapat Wongdee <panyapat12345@gmail.com>
2022-02-01 16:41:31 +0700

    lab21.cpp completed

--- lab21_1.cpp
+++ lab21_1.cpp
@@ -5,8 +5,13 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect a, Rect b){
+	double loc[4], side[2];
+	loc[0] = (a.x >= b.x)? a.x : b.x;
+	loc[1] = (a.x + a.w <= b.x + b.w)? a.x + a.w : b.x + b.w;
+	loc[2] = (a.y <= b.y)? a.y : b.y;
+	loc[3] = (a.y - a.h >= b.y - b.h)? a.y - a.h : b.y - b.h;
+	side[0] = loc[1] - loc[0];
+	side[1] = loc[2] - loc[3];
+	return (side[0] > 0 && side[1] > 0)? side[0]*side[1] : 0;
 }
